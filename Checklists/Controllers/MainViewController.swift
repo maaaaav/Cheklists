0bbaf1f649a2beb08d329ec1b112c40798c3d7db
maaaaav <aynulloevamavzuna@gmail.com>
2022-05-06 18:04:37 +0500

    added all done

--- Checklists/Controllers/MainViewController.swift
+++ Checklists/Controllers/MainViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 class MainViewController: UITableViewController {
 
     let groups:[CheckListGroup] = [
-        CheckListGroup (title: "Birthdays", imageName: "Birthdays", items:[ChecklistItem (isChecked: true, name: "congratulate Tomiris " ,remindMe: false)
+        CheckListGroup (title: "Birthdays", imageName: "Birthdays", items:[ChecklistItem (isChecked: true, name: "congratulate Tomiris " ,remindMe: false),ChecklistItem(isChecked: true, name: "Makhfirat", remindMe: true, dueDate: Date()), ChecklistItem(isChecked: true, name: "Mavzuna", remindMe: false)
             ]),
-        CheckListGroup(title: "Groceries", imageName: "Groceries",items:[ChecklistItem (isChecked: true, name: "ruurtr", remindMe: true, dueDate: Date())]),
+        CheckListGroup(title: "Groceries", imageName: "Groceries",items:[]),
         CheckListGroup(title: "To do", imageName: "Inbox", items:[ChecklistItem (isChecked: true, name: "cjfgjdd", remindMe: false)]),
         CheckListGroup(title: "Chores", imageName: "Chores", items:[ChecklistItem (isChecked: true, name: "ddrr", remindMe: true, dueDate: Date())])
                                                                          
@@ -31,6 +31,7 @@ class MainViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell( withIdentifier: "GroupCell", for: indexPath) as! GroupTableViewCell
         cell.titleLablel.text = group.title
         cell.iconeView.image = UIImage(named: group.imageName)
+        cell.subTitleLable.text = group.getRemainings()
         return cell
     }
     
--- Checklists/Models/CheckListGroup.swift
+++ Checklists/Models/CheckListGroup.swift
@@ -11,4 +11,26 @@ struct CheckListGroup{
     let title: String
     let imageName: String
     var items:[ChecklistItem]
-}
+    
+    func  getRemainings()-> String {
+        //1.All done
+        var isAllDone = true
+        for item in items {
+            if item.isChecked == false{
+                isAllDone = false
+            }
+        }
+        if items.isEmpty{
+            return "(No items)"
+        }else if !isAllDone, items.count != 0{
+            return"\(items.count)  Remaining"
+        }else{
+            if isAllDone{
+            return "All Done"
+        }else{
+            return "incorrect data"
+        }
+        }
+        }
+    }
+
